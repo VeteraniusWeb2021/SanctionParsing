1fcad2bf34f6787d2b1d055db1a7d0a8ac40d825
NikolaTeslaUA <limina.nika@gmail.com>
2022-01-18 16:37:25 +0200

    Add class Airplane

---
+++ SanctionParsing/SanctionParsing/Classes/Aiplane.py
@@ -0,0 +1,6 @@
+class Airplane():
+    def __init__(self, general_id, icao_code, manufacturer, serial_number):
+        self.genaral_id = general_id
+        self.icao_code = icao_code
+        self.manufacturer = manufacturer
+        self.serial_number = serial_number
