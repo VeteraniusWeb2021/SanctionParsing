20668f6878c3228e0cd56ccdfe1918c3c42012e5
kosta4310 <realanka77@gmail.com>
2022-01-25 23:11:42 +0200

    add fill table two options with ERRORS

--- SanctionParsing/dba/test/Script-6.sql
+++ SanctionParsing/dba/test/Script-6.sql
@@ -4,31 +4,8 @@ select * from sanctions.entities e ;
 delete from sanctions.entities ;
 
 		create table  temp_json (value text);
-		copy temp_json from 'D:\Downloads\veteranius\Entities.json';
-	select * from temp_json; 
-		drop table temp_json;
-		
-	BEGIN;
--- let's create a temp table to bulk data into
-create temporary table temp_json (values text) on commit drop;
-copy temp_json from program 'sed ''s/\\/\\\\/g'' <D:\Downloads\veteranius\test_csv.csv'>;
-
--- uncomment the line above to insert records into your table
--- insert into tbl_staging_eventlog1 ("EId", "Category", "Mac", "Path", "ID") 
-
-
-COMMIT;
-rollback;
---!!!!!!!!!!!!!!!
-
-delete from sanctions.entities ;
-create or replace procedure sanctions.sp_fill_entities_with_json()
-language plpgsql as
-$$
-begin
-		create temporary table  temp_json (value json) on commit drop;
 		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\Entities.json';
-		insert into sanctions.entities(
+	insert into sanctions.entities(
 				caption ,
 				datasets  ,
 				first_seen ,
@@ -46,7 +23,29 @@ begin
 						array[value->>'referents'],
 						value->>'schema',
 						value->>'target'
-							from temp_json);
-end;
-$$;
-call sanctions.sp_fill_entities_with_json();
+							from      (select value::json from temp_json)as t);
+--						
+--						SQL Error [22P02]: ОШИБКА: неверный синтаксис для типа json
+--  Detail: Неожиданный конец входной строки.
+--  Where: данные JSON, строка 1: ...-61"], "schema": "Organization", "target": true},
+	
+	
+	
+	
+	
+	select * from temp_json; 
+		drop table temp_json;
+		
+	BEGIN;
+-- let's create a temp table to bulk data into
+create temporary table temp_json (values text) on commit drop;
+copy temp_json from program 'sed ''s/\\/\\\\/g'' <D:\Downloads\veteranius\test_csv.csv'>;
+
+-- uncomment the line above to insert records into your table
+-- insert into tbl_staging_eventlog1 ("EId", "Category", "Mac", "Path", "ID") 
+
+
+COMMIT;
+rollback;
+--!!!!!!!!!!!!!!!
+
--- SanctionParsing/dba/test/fill_table_two_options.sql
+++ SanctionParsing/dba/test/fill_table_two_options.sql
@@ -1,9 +1,76 @@
+--заполняет одну таблицу из файла json к этой таблице(в from нужно указать абсолютный или относительный путь к файлу)
+
+drop procedure sanctions.sp_fill_entities_with_json();
+
+копи в json но вылезает ошибка
+
+create or replace procedure sanctions.sp_fill_entities_with_json()
+language plpgsql as
+$$
+begin
+		create temporary table  temp_json (value json) on commit drop;
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\Entities.json';
+		insert into sanctions.entities
+			(select 
+				value->>'caption',
+				array[value->>'datasets'],
+				value->>'first_seen',
+				value->>'id',
+				value->>'last_seen',
+				array[value->>'referents'],
+				value->>'schema',
+				value->>'target'
+					from temp_json);
+end;
+$$;
+
+call sanctions.sp_fill_entities_with_json();
+SQL Error [22P02]: ОШИБКА: неверный синтаксис для типа json
+  Detail: Неожиданный конец входной строки.
+  Where: данные JSON, строка 1: ...-61"], "schema": "Organization", "target": true},
+COPY temp_json, строка 1, столбец value: "[{"caption": "Ministu00e9rio do Interior", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:1..."
+SQL-оператор: "copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\Entities.json'"
+функция PL/pgSQL sanctions.sp_fill_entities_with_json(), строка 4, оператор SQL-оператор
+
+--!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Копи в текст потом разбираю в json но вылезает ошибка
+
+create table  temp_json (value text);
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\Entities.json';
+	insert into sanctions.entities(
+				caption ,
+				datasets  ,
+				first_seen ,
+				id ,
+				last_seen ,
+				referents  ,
+				schema ,
+				target )
+					(select 
+						value->>'caption',
+						array[value->>'datasets'],
+						value->>'first_seen',
+						value->>'id',
+						value->>'last_seen',
+						array[value->>'referents'],
+						value->>'schema',
+						value->>'target'
+							from      (select value::json from temp_json)as t);
+--						
+--						SQL Error [22P02]: ОШИБКА: неверный синтаксис для типа json
+--  Detail: Неожиданный конец входной строки.
+--  Where: данные JSON, строка 1: ...-61"], "schema": "Organization", "target": true}
+
+
+delete from sanctions.entities ;
+
+select * from sanctions.entities e ;
 
 create or replace procedure sanctions.sp_fill_entities_with_json(in_json_entities json)
 language plpgsql as
 $$
 begin
-insert into sanctions.entities (
+	insert into sanctions.entities (
 	caption,
 	datasets,
 	first_seen,
@@ -12,8 +79,8 @@ insert into sanctions.entities (
 	referents,
 	schema,
 	target)
-(select * from json_to_recordset
-	($1) AS x
+		(select * from json_to_recordset
+	($1) as x
 	(caption text,
 	datasets text array,
 	first_seen text,
@@ -22,7 +89,6 @@ insert into sanctions.entities (
 	referents text array,
 	schema text,
 	target text));
-	
 end;
 $$;
 
@@ -39,9 +105,4 @@ call sanctions.sp_fill_entities_with_json('[{"caption": "Minist\u00e9rio do Inte
 {"caption": "Abdulhai Salek", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2XAfkJ9t2UuWnoRG5EBoYu", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-780-23"], "schema": "Person", "target": true},
 {"caption": "Tarif AKHRAS", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2Yynnq5prSoe8NX8Zjb8i4", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2828-23"], "schema": "Person", "target": true},
 {"caption": "Vladimir Stepanovich ALEXSEYEV", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2ZUrGJDocRY3AQBHSbpKg5", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-4908-14"], "schema": "Person", "target": true}]');
-select * from sanctions.entities e ;
-
---second option!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 
