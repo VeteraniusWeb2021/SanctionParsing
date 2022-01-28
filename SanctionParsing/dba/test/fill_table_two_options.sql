9b7efd75e2acdbeae2ca56574cb0244d2778dee6
kosta4310 <realanka77@gmail.com>
2022-01-28 20:33:43 +0200

    add sample ok json,save from database file json with properties

--- SanctionParsing/dba/test/fill_table_two_options.sql
+++ SanctionParsing/dba/test/fill_table_two_options.sql
@@ -1,7 +1,6 @@
 --заполняет одну таблицу из файла json к этой таблице(в from нужно указать абсолютный или относительный путь к файлу)
 
-копи в json ,работает с экранированными символами не работает с массивом по полю. нужно в поля с массивом добавить
-обработку json array
+копи в json ,работает с экранированными символами with file type .txt
 
 drop schema sanctions cascade;
 create schema sanctions;
@@ -12,7 +11,7 @@ language plpgsql as
 $$
 begin
 		create temporary table  temp_json (value json) on commit drop;
-		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\modify.json';
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\test\test_json.txt';
 		insert into sanctions.entities(
 				caption ,
 				datasets  ,
@@ -24,11 +23,11 @@ begin
 				target )
 			(select 
 				value->>'caption',
-				array[value->>'datasets'],
+				array[json_array_elements_text (value->'datasets')],
 				value->>'first_seen',
 				value->>'id',
 				value->>'last_seen',
-				array[value->>'referents'],
+				array[json_array_elements_text (value->'referents')],
 				value->>'schema',
 				value->>'target'
 					from temp_json);
@@ -36,6 +35,7 @@ end;
 $$;
 
 call sanctions.sp_fill_entities_with_json();
+select * from sanctions.entities e ;
 
 
 Работает с неэкранированными символами т.е. с одним бэкслешем и с массивом "datasets": ["eu_fsf","eu"]
@@ -65,18 +65,6 @@ begin
 	target text));
 end;
 $$;
-
-call sanctions.sp_fill_entities_with_json('[{"caption": "Minist\u00e9rio do Interior", "datasets": ["eu_fsf","eu"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2QtbU49vp9LkfKRjd8WQni", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-3074-61"], "schema": "Organization", "target": true},
-{"caption": "Fajr Aviation Composite Industries", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-28X5jMopMz2jCXsUDUJczU", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2001-61"], "schema": "Organization", "target": true},
-{"caption": "Sergei Ivanovich MENYAILO", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2CHqNtWeErMHi5i8RPCqC7", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-5896-7"], "schema": "Person", "target": true},
-{"caption": "Abdolsamad KHORAMABADI", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2DzyaDxjobuVwcDTps4VSJ", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2978-90"], "schema": "Person", "target": true},
-{"caption": "Samuel FERNANDES", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2EnNuyDr5SWKXLH8bpSjok", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-3021-64"], "schema": "Person", "target": true},
-{"caption": "Igor Viacheslavovich LIUBOVITSKI", "datasets": ["eu_fsf"], "first_seen": "2021-12-04T03:03:14", "id": "NK-2Mdk9YADKMyneyjdhWMtwn", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-6701-76"], "schema": "Person", "target": true},
-{"caption": "IRGC", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2PacmNpiLUmoz4XD2mSt7A", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2078-17"], "schema": "Organization", "target": true},
-{"caption": "Korea United Development Bank", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2Qo8ydZ5zPR5Se5GidfVsG", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-4103-55"], "schema": "Organization", "target": true},
-{"caption": "Al-Kaida v Iraku", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2T3S8Hu29ktYnKSTgQp2wL", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-1163-58"], "schema": "Organization", "target": true},
-{"caption": "MATSA (Mohandesi Toseh Sokht Atomi Company)", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2VSxJneqvnUDT2D5iv2i2x", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2649-60"], "schema": "Organization", "target": true},
-{"caption": "Abdulhai Salek", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2XAfkJ9t2UuWnoRG5EBoYu", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-780-23"], "schema": "Person", "target": true},
-{"caption": "Tarif AKHRAS", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2Yynnq5prSoe8NX8Zjb8i4", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-2828-23"], "schema": "Person", "target": true},
-{"caption": "Vladimir Stepanovich ALEXSEYEV", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2ZUrGJDocRY3AQBHSbpKg5", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-4908-14"], "schema": "Person", "target": true}]');
-
+select * from sanctions.entities e ;
+call sanctions.sp_fill_entities_with_json('[{"caption": "Vladimir Stepanovich ALEXSEYEV", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2ZrGocoY3llAQBHSbpKg5", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-4908-14"], "schema": "Person", "target": true},
+{"caption": "Vladimir Stepnovich ALEXSEYEV", "datasets": ["eu_fsf"], "first_seen": "2021-09-26T14:52:11", "id": "NK-2ZrGJDokkcRYQBHpbpKg5", "last_seen": "2021-12-19T03:03:11", "referents": ["eu-fsf-eu-4908-14"], "schema": "Person", "target": true}]');
