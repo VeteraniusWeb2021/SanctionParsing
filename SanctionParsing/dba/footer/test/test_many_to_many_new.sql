06d2a2d435a1354cca8ed4e6d0216902351c8b35
kosta4310 <realanka77@gmail.com>
2022-02-01 21:11:08 +0200

    ordering

--- SanctionParsing/dba/footer/test/test_many_to_many_new.sql
+++ SanctionParsing/dba/footer/test/test_many_to_many_new.sql
@@ -1,41 +1,42 @@
- create table sanctions.person_caption
+ create table public.person_caption
  (person_id varchar,
  caption_id varchar,
  primary key (person_id,caption_id),
- foreign key (person_id) references sanctions.person(general_id),
- foreign key (caption_id) references sanctions.entities(id)
+ foreign key (person_id) references public.person(general_id),
+ foreign key (caption_id) references public.entities(id)
  );
  
-insert into sanctions.person_caption values 
-('NK-2QtbU49vp9LkfKRjd8WQni','addr-04a17d22124400ad9222dd03a9c8175ae1815669'),
-('NK-2QtbU49vp9LkfKRjd8WQni','addr-05fa58176c94fea95eda5b89e2a6296da9e4eebf'),
+insert into public.person_caption values 
+('NK-2QtbU49vp9LkfKRjd8WQni','NK-2CHqNtWeErMHi5i8RPCqC7'),
+('NK-2QtbU49vp9LkfKRjd8WQni','NK-2ZUrGJDocRY3AQBHSbpKg5');
+
 ('addr-007fdb0e3654fc794a3f4e347f5b68882dbf3df5','addr-06498413e23bd8d4119519f68f303aef376785eb'),
 ('addr-007fdb0e3654fc794a3f4e347f5b68882dbf3df5','addr-068f144b51f04dc969702315a48947ce55b5c8cd');
 
-select p.*,e.caption ,e.first_seen ,e.referents ,e.schema ,e.target from sanctions.person p
-join sanctions.person_caption pc on pc.person_id = p.general_id 
-join sanctions.entities e on e.id = pc.caption_id ;
+select p.*,e.caption ,e.first_seen ,e.referents ,e.schema ,e.target from public.person p
+join public.person_caption pc on pc.person_id = p.general_id 
+join public.entities e on e.id = pc.caption_id ;
 
-select p.caption ,p.first_seen ,p.general_id ,p.country ,array_agg(e.caption) ,array_agg(e.first_seen)from sanctions.person p
-join sanctions.person_caption pc on pc.person_id = p.general_id 
-join sanctions.entities e on e.id = pc.caption_id 
+select p.caption ,p.first_seen ,p.general_id ,p.country ,array_agg(e.caption) ,array_agg(e.first_seen)from public.person p
+join public.person_caption pc on pc.person_id = p.general_id 
+join public.entities e on e.id = pc.caption_id 
 group by p.caption ,p.first_seen ,p.general_id ,p.country ;
 
 select row_to_json(t) from  (select p.caption ,p.first_seen ,p.general_id ,p.country, (array_agg(e.caption) ,array_agg(e.first_seen) ) as properties
-from sanctions.person p
-join sanctions.person_caption pc on pc.person_id = p.general_id 
-join sanctions.entities e on e.id = pc.caption_id 
+from public.person p
+join public.person_caption pc on pc.person_id = p.general_id 
+join public.entities e on e.id = pc.caption_id 
 group by p.caption ,p.first_seen ,p.general_id ,p.country ) as t;
 
 copy (select row_to_json(t) from  (select p.caption ,p.first_seen ,p.general_id ,p.country ,(array_agg(e.caption) ,array_agg(e.first_seen) ) as properties
-from sanctions.person p
-join sanctions.person_caption pc on pc.person_id = p.general_id 
-join sanctions.entities e on e.id = pc.caption_id 
+from public.person p
+join public.person_caption pc on pc.person_id = p.general_id 
+join public.entities e on e.id = pc.caption_id 
 group by p.caption ,p.first_seen ,p.general_id ,p.country ) as t) 
 to 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\test\ok_json.json';
 
 
-select p.general_id ,unnest (p.country) from sanctions.person p ;
+select p.general_id ,unnest (p.country) from public.person p ;
 
 
 
--- SanctionParsing/dba/footer/test/test_many_to_many_old.sql
+++ SanctionParsing/dba/footer/test/test_many_to_many_old.sql
@@ -1,54 +1,104 @@
 --create schema data_oceon_sanction_parsing;
-create schema sanctions;
 
-select * from sanctions.entities e ;
-delete from sanctions.person;
-drop table sanctions.person;
 
-create table sanctions.person 
+select * from public.entities e ;
+delete from public.person;
+drop table public.person;
+
+create table public.person 
 (caption varchar,
 first_seen varchar,
 general_id varchar,
 country varchar[],
 primary key (general_id),
-foreign key (general_id) references sanctions.entities(id));
+foreign key (general_id) references public.entities(id));
 
-create table sanctions.country 
+create table public.entities
+(id_int serial,
+caption text,
+datasets text array,
+first_seen text,
+id text,
+last_seen text,
+referents text array,
+schema text,
+target boolean,
+unique(id_int),
+primary key (id)
+);
+
+
+create or replace procedure public.sp_fill_entities_with_json()
+language plpgsql as
+$$
+begin
+		create temporary table  temp_json (value json) on commit drop;
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\footer\test\sample_target.txt';
+		insert into public.entities(
+				caption ,
+				datasets  ,
+				first_seen ,
+				id ,
+				last_seen ,
+				referents  ,
+				schema ,
+				target )
+			(select 
+				value->>'caption',
+				array (select json_array_elements_text (value->'datasets')) as dtsets,
+				value->>'first_seen',
+				value->>'id',
+				value->>'last_seen',
+				array (select json_array_elements_text (value->'referents')) as rfs,
+				value->>'schema',
+				(value->>'target')::boolean
+					from temp_json );
+end;
+$$;
+rollback;
+
+call public.sp_fill_entities_with_json();
+
+select * from public.entities e ;
+
+
+
+create table public.country 
 (code varchar,
 label varchar,
 primary key (code));
 
-create table sanctions.country_person
+create table public.country_person
 (id_person varchar,
 code_country varchar,
 primary key(id_person,code_country),
-foreign key (id_person) references sanctions.person(general_id),
-foreign key (code_country) references sanctions.country(code));
+foreign key (id_person) references public.person(general_id),
+foreign key (code_country) references public.country(code));
 
-insert into sanctions.person values ('Minist?rio do Interior','2021-09-26T14:52:11','NK-2QtbU49vp9LkfKRjd8WQni','{ac,ad}'),
-									('Fajr Aviation Composite Industries','2021-09-26T14:52:11','addr-007fdb0e3654fc794a3f4e347f5b68882dbf3df5','{am,ao}');
+insert into public.person values ('Minist?rio do Interior','2021-09-26T14:52:11','NK-2QtbU49vp9LkfKRjd8WQni','{ac,ad}'),
+									('Fajr Aviation Composite Industries','2021-09-26T14:52:11','NK-2XAfkJ9t2UuWnoRG5EBoYu','{am,ao}');
 
-select  * from sanctions.person;
+select  * from public.person;
 
-copy sanctions.country(code,label) from 
+copy public.country(code,label) from 
 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\data\data_for_tables\country_utf-8.csv' (delimiter ';',format csv);
 
-select * from sanctions.country;
-select * from sanctions.country_person cp ;
+select * from public.country;
+select * from public.country_person cp ;
 
-insert into sanctions.country_person values ('NK-2QtbU49vp9LkfKRjd8WQni','ac'),('NK-2QtbU49vp9LkfKRjd8WQni','ad'),
+insert into public.country_person values ('NK-2QtbU49vp9LkfKRjd8WQni','ac'),('NK-2QtbU49vp9LkfKRjd8WQni','ad'),
 ('NK-28X5jMopMz2jCXsUDUJczU','am'),('NK-28X5jMopMz2jCXsUDUJczU','ao');
 
 
-copy (select json_agg(t3.*) from
-(select * from sanctions.person p
-join sanctions.entities e on e.id =p.general_id 
-join 
-(select c.label as country, t1.general_id from sanctions.country c 
-join
-(select cp.code_country,p.general_id from sanctions.country_person cp 
-join sanctions.person p on p.general_id=cp.id_person)t1 on t1.code_country=c.code)t2 on t2.general_id = e.id)t3)
-to 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\test\test_out_json.json' ;
+--copy (select json_agg(t3.*) from
+--(select * from .person p
+--join .entities e on e.id =p.general_id 
+--join 
+--(select c.label as country, t1.general_id from .country c 
+--join
+--(select cp.code_country,p.general_id from .country_person cp 
+--join .person p on p.general_id=cp.id_person)t1 on t1.code_country=c.code)t2 on t2.general_id = e.id)t3)
+--to 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\test\test_out_json.json' ;
 
 
 
