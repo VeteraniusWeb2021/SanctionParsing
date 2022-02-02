4e77d1b75c7a6f2532a57d8edb08a980c66b44e7
kosta4310 <realanka77@gmail.com>
2022-02-02 20:56:47 +0200

    add fillig table thing

--- SanctionParsing/dba/fill_head_tables.sql
+++ SanctionParsing/dba/fill_head_tables.sql
@@ -5,7 +5,7 @@ language plpgsql as
 $$
 begin
 		create temporary table  temp_json (value json) on commit drop;
-		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\data\data_for_tables\entities_test.txt';
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\data\data_for_tables\entities.txt';
 		insert into sanctions.entities(
 				caption ,
 				datasets  ,
@@ -24,18 +24,20 @@ begin
 				array (select json_array_elements_text (value->'referents')) as rfs,
 				value->>'schema',
 				(value->>'target')::boolean
-					from temp_json );
+					from temp_json ) on conflict(id) do nothing;
 end;
 $$;
 
 call sanctions.sp_fill_entities_with_json();
+select count(*) from sanctions.entities e ;
 --/////////////////////////////////////////////////////////////////////
+
 create or replace procedure sanctions.sp_fill_thing_with_json()
 language plpgsql as
 $$
 begin
 		create temporary table  temp_json (value json) on commit drop;
-		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\data\data_for_tables\thing_test.txt';
+		copy temp_json from 'D:\Downloads\veteranius\veteranius-vcs\vcs\SanctionParsing\SanctionParsing\dba\data\data_for_tables\thing.txt';
 		insert into sanctions.thing(
 				general_id ,
 				address,
