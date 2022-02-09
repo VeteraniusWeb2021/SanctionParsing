d154a68567d7ee7b778f30b4045d2a9eb7b3f827
Kostiantyn Pavlov <realanka77@gmail.com>
2022-02-09 23:34:16 +0200

    created filling some addtables

--- SanctionParsing/dba/fill_addtables.sql
+++ SanctionParsing/dba/fill_addtables.sql
@@ -11,5 +11,26 @@ INSERT INTO sanctions.legalEntity_cryptoWallets
 INSERT INTO sanctions.legalEntity_directorships
 (select e.general_id ,unnest (e.directorshipDirector) from sanctions.legalentity e); 
 
-INSERT INTO sanctions.legalEntity_identification
-(select e.general_id ,unnest (e.identificiation) from sanctions.legalentity e);
+--INSERT INTO sanctions.legalEntity_identification
+--(select e.general_id ,unnest (e.identificiation) from sanctions.legalentity e);
+
+INSERT INTO sanctions.legalEntity_jurisdiction
+(select e.general_id ,unnest (e.jurisdiction) from sanctions.legalentity e);
+
+INSERT INTO sanctions.legalEntity_mainCountry
+(select e.general_id ,unnest (e.mainCountry) from sanctions.legalentity e);
+
+
+INSERT INTO sanctions.legalEntity_membershipMember
+(select e.general_id ,unnest (e.membershipMember) from sanctions.legalentity e);
+
+INSERT INTO sanctions.legalEntity_operatedVehicles
+(select e.general_id ,unnest (e.operatedVehicles) from sanctions.legalentity e);
+
+INSERT INTO sanctions.legalEntity_ownedVehicles
+(select e.general_id ,unnest (e.ownedVehicles) from sanctions.legalentity e);
+
+INSERT INTO sanctions.legalEntity_ownershipOwner
+(select e.general_id ,unnest (e.ownershipOwner) from sanctions.legalentity e);
+
+
